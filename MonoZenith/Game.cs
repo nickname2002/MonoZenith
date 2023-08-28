b2478e81b5128843a04e3eb8a34908369b1b0ca1
Nick Jordan <nick@jorcademy.nl>
2023-08-28 17:42:28 +0200

    Minor changes.

--- MonoZenith/Game.cs
+++ MonoZenith/Game.cs
@@ -1,17 +1,12 @@
-using System.Diagnostics;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 namespace MonoZenith;
 
 public partial class Game
 {
-    private SpriteFont pixelFont;
-
     /* Initialize game vars and load assets. */
     public void Init()
     {
-        pixelFont = LoadFont("pixel");
+        
     }
 
     /* Update game logic. */
@@ -23,6 +18,6 @@ public partial class Game
     /* Draw objects/backdrop. */
     public void Draw()
     {
-        DrawText("Hello, World!", new Vector2(100, 100), pixelFont, Color.White);
+        
     }
 }
