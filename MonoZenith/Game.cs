08da80d6f64cd28fd4a34f7e48d459f18bab09b4
Nick Jordan <nick@jorcademy.nl>
2023-08-28 11:39:28 +0200

    Some changes made.

--- MonoZenith/Game.cs
+++ MonoZenith/Game.cs
@@ -5,21 +5,23 @@ namespace MonoZenith;
 
 public partial class Game
 {
+    private SpriteFont pixelFont;
+
     /* Initialize game vars and load assets. */
     public void Init()
     {
-
+        pixelFont = LoadFont("pixel");
     }
 
     /* Update game logic. */
     public void Update(GameTime deltaTime)
     {
-
+        
     }
     
     /* Draw objects/backdrop. */
     public void Draw()
     {
-
+        DrawText("Hello, World!", new Vector2(100, 100), pixelFont, Color.White);
     }
 }
