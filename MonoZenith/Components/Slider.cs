cbc43e0481a72bd547e07cf44ff415489cfe97b2
Nick Jordan <nick@jorcademy.nl>
2023-09-03 07:55:18 +0200

    Implemented Slider component.

---
+++ MonoZenith/Components/Slider.cs
@@ -0,0 +1,114 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoZenith.Components;
+
+public class Slider : Component
+{
+    // Slider
+    public float Min { get; set; }
+    public float Max { get; set; }
+    public float Value { get; set; }
+    
+    // Color
+    public Color SliderColor { get; set; }
+    public Color BarColor { get; set; }
+    
+    // Value
+    public int DecimalPlaces { get; set; }
+    public float TextScale { get; set; }
+    public Color TextColor { get; set; }
+    
+    private bool _sliding;
+    private SpriteFont _font;
+    
+    public Slider(Game g, Vector2 pos, int width, int height, int min, int max) 
+        : base(g, pos, width, height)
+    {
+        Min = min;
+        Max = max;
+        Value = (max + min) / 2.0f;
+     
+        SliderColor = Color.Black;
+        BarColor = Color.DarkGray;
+        DecimalPlaces = 1;
+        
+        TextScale = 2;
+        TextColor = Color.Black;
+        
+        _sliding = false;
+        _font = g.LoadFont("pixel");
+    }
+
+    /* Check if mouse is hovering over slider. */
+    private bool IsHovering()
+    {
+        return Game.GetMousePosition().X > Position.X - Width / 2
+            && Game.GetMousePosition().X < Position.X + Width / 2
+            && Game.GetMousePosition().Y > Position.Y - Height / 2
+            && Game.GetMousePosition().Y < Position.Y + Height / 2;
+    }
+    
+    /* Cap value between min and max. */
+    private void CapValue()
+    {
+        if (Value < Min)
+            Value = Min;
+        else if (Value > Max)
+            Value = Max;
+    }
+    
+    public override void Update(GameTime deltaTime)
+    {
+        if (_sliding && !Game.GetMouseButtonDown(MouseButtons.Left))
+            _sliding = false;
+        
+        if (!IsHovering() && !_sliding) 
+            return;
+
+        if (!Game.GetMouseButtonDown(MouseButtons.Left)) 
+            return;
+        
+        _sliding = true;
+        Value = (Game.GetMousePosition().X - Position.X + Width / 2) * (Max - Min) / Width + Min;
+        CapValue();
+    }
+
+    /* Draw slider bar. */
+    private void DrawBar()
+    {
+        Game.DrawRectangle(BarColor, Position, Width, Height);
+    }
+    
+    /* Draw slider. */
+    private void DrawSlider()
+    {
+        int sliderWidth = 15;
+        int sliderHeight = Height * 4;
+        int sliderX = (int)(Position.X - Width / 2 + Width * (Value - Min) / (Max - Min));
+        Game.DrawRectangle(
+            SliderColor, 
+            new Vector2(sliderX, Position.Y), 
+            sliderWidth, 
+            sliderHeight);
+    }
+
+    private void DrawProperties()
+    {
+        int sliderX = (int)(Position.X - Width / 2 + Width * (Value - Min) / (Max - Min));
+        Game.DrawText(
+            Math.Round(Value, DecimalPlaces).ToString(), 
+            new Vector2(sliderX, Position.Y - Height / 2 - 40), 
+            _font, 
+            TextColor, 
+            TextScale);
+    }
+    
+    public override void Draw()
+    {
+        DrawBar();
+        DrawSlider();
+        DrawProperties();
+    }
+}
--- MonoZenith/Game.cs
+++ MonoZenith/Game.cs
@@ -6,21 +6,26 @@ namespace MonoZenith;
 
 public partial class Game
 {
+    private Slider s;
+    
     /* Initialize game vars and load assets. */
     public void Init()
     {
-
+        SetScreenSize(800, 600);
+        SetBackgroundColor(Color.White);
+        s = new Slider(this, new Vector2(400, 300), 500, 10, 0, 1);
+        s.DecimalPlaces = 1;
     }
 
     /* Update game logic. */
     public void Update(GameTime deltaTime)
     {
-
+        s.Update(deltaTime);
     }
     
     /* Draw objects/backdrop. */
     public void Draw()
     {
-
+        s.Draw();
     }
 }
