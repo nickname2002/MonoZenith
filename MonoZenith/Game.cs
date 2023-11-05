43a3daaaaa697d483a526434fdb4112ef1ae8ffa
Nick Jordan <nick@jorcademy.nl>
2023-11-05 15:38:28 +0100

    Cleaned code.

--- MonoZenith/Game.cs
+++ MonoZenith/Game.cs
@@ -8,14 +8,10 @@ namespace MonoZenith;
 
 public partial class Game
 {
-    private Texture2D _tileMap;
-    private Texture2D _tile;
-    
     /* Initialize game vars and load assets. */
     public void Init()
     {
-        _tileMap = ReadTileMap("Textures/tilemap.png");
-        _tile = ReadTileFromMap(_tileMap, 1, 32);
+
     }
 
     /* Update game logic. */
@@ -27,6 +23,6 @@ public partial class Game
     /* Draw objects/backdrop. */
     public void Draw()
     {
-        DrawImage(_tile, new Vector2(16, 16));
+        
     }
 }
