564f0dfa1165b670eb3dc344f7c8869cf9c7adc2
nickname2002 <nickjordan2002@gmail.com>
2023-12-24 07:38:09 +0100

    Cleaned Game.cs.

--- MonoZenith/Game.cs
+++ MonoZenith/Game.cs
@@ -8,33 +8,21 @@ namespace MonoZenith;
 
 public partial class Game
 {
-    private TextField _textField;
-    
     /* Initialize game vars and load assets. */
     public void Init()
     {
-        SetScreenSize(800, 600);
-        SetBackgroundColor(Color.White);
         
-        // Create text field
-        _textField = new TextField(this, new Vector2(400, 300), 210, 50, 1)
-        {
-            BackColor = Color.DarkGray,
-            ContentColor = Color.White,
-            BorderColor = Color.DimGray,
-            MaxLength = 20
-        };
     }
 
     /* Update game logic. */
     public void Update(GameTime deltaTime)
     {
-        _textField.Update(deltaTime);
+        
     }
     
     /* Draw objects/backdrop. */
     public void Draw()
     {
-        _textField.Draw();
+        
     }
 }
